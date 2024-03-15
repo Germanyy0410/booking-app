dcd7667d39d1efaf630fea31ceab5cc3939b756b
Germanyy0410 <tienduchoang136@gmail.com>
2024-03-15 15:37:17 +0700

    update url

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const jwtSecret = "qiwuerdioajhsncfkxznvlkd";
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://quiet-pasca-2a75d2.netlify.app/",
   })
 );
 
